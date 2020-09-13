91286913a62ce444b5f3cddaf16b7bdf23c962a0
johanesrafael <rafliong95@gmail.com>
2020-09-13 23:07:35 +0800

    Fixed package issues

---
+++ src/main/java/Circus/stuff/Cage.java
@@ -0,0 +1,16 @@
+package Circus.stuff;
+
+// Cage
+public class Cage<T> {
+    private T t;
+
+    public void lockUp(T t) {
+        this.t = t;
+        System.out.println("In the cage: " + t);
+    }
+
+    public T release() {
+        System.out.println(t + " out of cage");
+        return t;
+    }
+}
--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -5,5 +5,6 @@ import Circus.Asset;
 public abstract class Animal implements Asset {
 
     public String name;
+
     public abstract String speak();
 }
